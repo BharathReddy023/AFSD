a1f28c897e6677414df0777ba76cb5ac61effde5
bharathreddycse2022 <bharathreddycse2022@gmail.com>
2023-07-23 21:32:33 +0530

    Register

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
-const express = require("express")
-const app=express()
-const port=4000
-app.get('/',(req,res) =>{
-    res.send("Hello TalentSprint")
-})
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-app.listen(port,() =>{
-    console.log("server listening on 4000")
-})
+const app = express();
+const PORT = 5000; // Change this to any available port you prefer
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/api/register', (req, res) => {
+  const { name, email, password,e_balance } = req.body;
+  // You can perform registration logic here, like saving data to a database
+  console.log('Received registration data:', name, email, password,e_balance);
+  res.status(200).send('Registration successful!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
